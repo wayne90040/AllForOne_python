23e3e403a3d484d16d54058c538efc2c9cd078c5
WeiLun <wayne90040@gmail.com>
2020-07-21 22:40:39 +0800

    AllForOne v1.0

--- AllForOne_Back/wsgi.py
+++ AllForOne_Back/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling  # For Heroku
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AllForOne_Back.settings')
 
 application = get_wsgi_application()
+
+application = Cling(get_wsgi_application())  # For Heroku
